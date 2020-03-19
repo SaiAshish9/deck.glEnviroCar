c1bd89556fdb9031c80f75918a111ebfdc15959c
SaiAshish9 <saiashish3760@gmail.com>
2020-03-20 04:26:05 +0530

    Updates

---
+++ precache-manifest.2fae1e04110a4d1adeaf005b318f218b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e6e5d8ba447d94def859744f1341c9",
+    "url": "/deck.glEnviroCar/index.html"
+  },
+  {
+    "revision": "8727fb6df60de242dbb8",
+    "url": "/deck.glEnviroCar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3b8507d93fa932b75a92",
+    "url": "/deck.glEnviroCar/static/js/2.f9b31716.chunk.js"
+  },
+  {
+    "revision": "7bfcea31855bd57860628e5726359b29",
+    "url": "/deck.glEnviroCar/static/js/2.f9b31716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8727fb6df60de242dbb8",
+    "url": "/deck.glEnviroCar/static/js/main.57dfa5f8.chunk.js"
+  },
+  {
+    "revision": "695c575e63a072967d52",
+    "url": "/deck.glEnviroCar/static/js/runtime-main.89c9c902.js"
+  }
+]);
